adca6a6db35adb5f270987bcc640be6e7f4847d9
Muhammed Talha Korkmaz <kriptovekediler@gmail.com>
2024-06-22 18:51:16 +0300

    config adjustments

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  reactStrictMode: true,
+  images: {
+    domains: [
+      "images.unsplash.com",
+      "tailwindui.com",
+      "res.cloudinary.com",
+      "imgbb.com",
+    ],
+  },
+};
 
 export default nextConfig;
