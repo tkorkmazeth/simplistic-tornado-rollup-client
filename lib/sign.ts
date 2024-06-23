acd31ed47448cb39d4ae658d132678ce1db30c02
Muhammed Talha Korkmaz <kriptovekediler@gmail.com>
2024-06-23 23:06:30 +0300

    lib

--- lib/sign.ts
+++ lib/sign.ts
@@ -9,6 +9,10 @@ import {
 } from "@ethsign/sp-sdk";
 import { privateKeyToAccount } from "viem/accounts";
 import { SchemaEntry } from "./types";
+import { BrowserProvider, JsonRpcSigner } from "ethers";
+import { useMemo } from "react";
+import type { Account, Chain, Client, Transport } from "viem";
+import { type Config, useConnectorClient } from "wagmi";
 
 var id = crypto.randomBytes(32).toString("hex");
 const privateKey: any = `0x${id}`;
@@ -67,3 +71,20 @@ export const getAttestations = async (page: number, indexingValue?: string) => {
     page: page,
   });
 };
+
+export function clientToSigner(client: Client<Transport, Chain, Account>) {
+  const { account, chain, transport } = client;
+  const network = {
+    chainId: chain.id,
+    name: chain.name,
+    ensAddress: chain.contracts?.ensRegistry?.address,
+  };
+  const provider = new BrowserProvider(transport, network);
+  const signer = new JsonRpcSigner(provider, account.address);
+  return signer;
+}
+
+export function useSigner({ chainId }: { chainId?: number } = {}) {
+  const { data: client } = useConnectorClient<Config>({ chainId });
+  return useMemo(() => (client ? clientToSigner(client) : undefined), [client]);
+}
